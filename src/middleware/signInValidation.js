0d7e960f76da325e9f2046b0c473d189f0af3743
Vini-P <viniciuspedrosoreis@gmail.com>
2023-03-02 12:06:47 -0300

    SignIn & SignUp

--- src/middleware/signInValidation.js
+++ src/middleware/signInValidation.js
@@ -1,17 +1,31 @@
+import { connectionDB } from "../database";
 import signInSchema from "../schemas/signInSchema";
 
-export default function signInValidation (req, res, next){
+export default async function signInValidation (req, res, next){
     const {signInData} = req.body;
 
     const signInError = signInSchema.validate(signInData);
 
     if (signInError){
-        // config errors
-        // 
-        return;
+        const errors = signInError.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
     } else{
         res.status(200);
     }
 
-    next()
+    try {
+        const userExist = await connectionDB.query(`SELECT email FROM users WHERE email = ${signInData.email}`)
+        if (!userExist){
+            return res.sendStatus(404)
+        }
+
+        const passwordCheck = bcrypt.compareSync(signInData.password, userExist.password);
+        if (!passwordCheck) {
+            return res.sendStatus(401);
+        }
+        next()
+    
+    }catch{
+        res.status(500).send(error.message);
+    }
 }
